ebc6c0711b06dcfcd61d69b8c4e9de85130affb3
Naseem Emustafa <nazeem5438>
2020-11-13 15:51:17 -0600

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,22 @@
+public class HelloWorld{
+	public static void main(String args[]){
+		if(args.length > 0){
+			int max = Integer.parseInt(args[0]);
+			int min = Integer.parseInt(args[0]);
+			for(String ob : args){
+				int no2 = Integer.parseInt(ob);
+				if(max < no2){
+					max = no2;
+				}
+				if(min > no2){
+					min = no2;
+				}				
+			}
+			System.out.println("The max number is " + max);
+			System.out.println("The min number is " + min);
+		}else{
+			System.out.println("There is no number to find max in the input array");
+		}
+		
+	}
+}
