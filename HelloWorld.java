5ce8a4cbb9a383c73b0098e05f270169d25541d1
Naseem Emustafa <nazeem5438>
2020-11-13 16:10:15 -0600

    second commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,9 @@ public class HelloWorld{
 		if(args.length > 0){
 			int max = Integer.parseInt(args[0]);
 			int min = Integer.parseInt(args[0]);
+			System.out.println("Input Array ");
 			for(String ob : args){
+				System.out.print(obj);
 				int no2 = Integer.parseInt(ob);
 				if(max < no2){
 					max = no2;
@@ -12,6 +14,7 @@ public class HelloWorld{
 					min = no2;
 				}				
 			}
+			
 			System.out.println("The max number is " + max);
 			System.out.println("The min number is " + min);
 		}else{
